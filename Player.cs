f19d4a5150c2dc1d1f1587554c5e978f0d8928fd
Alicja Wilińska <72753653+awilinska@users.noreply.github.com>
2022-10-21 17:32:04 +0200

    1.0.1
    Animation update, player rotation.

--- Player.cs
+++ Player.cs
@@ -11,12 +11,15 @@ public class Player : MonoBehaviour
     public float health = 1;
     public float aura = 0;
     public Animator animate;
+    private SpriteRenderer sprite;
+    public GameObject transformerSound;
 
     public Sprite aura_one;
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -30,18 +33,33 @@ public class Player : MonoBehaviour
 
         move = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(movementSpeed * move, body.velocity.y);
-        animate.SetFloat("movementSpeed", Mathf.Abs(move));
 
         if (Input.GetKeyDown(KeyCode.Space)) {
             body.AddForce(new Vector2(body.velocity.x, jump));
         }
+
+        AnimationUpdate();
     }
 
         void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Transformer")) {
+            if (aura == 0) {
+                Instantiate(transformerSound, transform.position, Quaternion.identity);
+            }
             this.gameObject.GetComponent<SpriteRenderer>().sprite = aura_one;
             aura = 1;
             animate.SetBool("Green", true);
+        }
+    }
+
+    void AnimationUpdate()  {
+        if (move > 0f) {
+            animate.SetFloat("movementSpeed", Mathf.Abs(move));
+            sprite.flipX = false;
+        }
+        else if (move < 0f) {
+            animate.SetFloat("movementSpeed", Mathf.Abs(move));
+            sprite.flipX = true;
         }
     }
 }

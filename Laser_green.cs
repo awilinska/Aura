bd33eee267fd55b8c301cbf1631d689f7c38bf86
Alicja Wilińska <72753653+awilinska@users.noreply.github.com>
2022-10-24 22:01:03 +0200

    1.0.1
    Adding more lasers

--- Laser_green.cs
+++ Laser_green.cs
@@ -2,25 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Laser : MonoBehaviour
+public class Laser_green : MonoBehaviour
 {
-    // // Start is called before the first frame update
 
     public float laserDamage = 1;
-    // void Start()
-    // {
-        
-    // }
-
-    // // Update is called once per frame
-    // void Update()
-    // {
-        
-    // }
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")) {
-            if (other.GetComponent<Player>().aura == 0) {
+            if (other.GetComponent<Player>().aura != 1) {
                 other.GetComponent<Player>().health -= laserDamage;
                 Debug.Log("Btoom");
             }
